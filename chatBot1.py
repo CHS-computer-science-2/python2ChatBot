140341a86171b2c2c116de5376295ab63f26586c
Dunelandsue <32044355+Dunelandsue@users.noreply.github.com>
2019-01-24 12:33:45 -0600

    Create chatBot1.py
    Initial Code

---
+++ chatBot1.py
@@ -0,0 +1,14 @@
+#This is the starter code for the Chatbot 
+# <Your Name Here>
+#This is a chatbot and this is a comment, not executed by the program
+#Extend it to make the computer ask for your favorite movie and respond accordingly!
+
+print('Hello this is your computer...what is your favorite number?')
+#Declaring our first variable below called 'computerfavnumber' and storing the value 33
+computerfavnumber=33
+#We now declare a variable but set the variable to hold whatever the *user* inputs into the program
+favnumber=input()
+print(favnumber + '...is a very nice number indeed. So...what is your name?')
+name=input()
+print('what a lovely name:  ' + name + '...now, I will reveal what my favourite number is:')
+print (computerfavnumber)
